5da02c375b2c6b415b218e49540c5ec46da518d5
Sairan Serra dos Santos <sairan.serra@gmail.com>
2024-12-12 14:28:00 -0300

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <Styled.ContainerMaster>
       <Styled.WrapperText>
         <Styled.Icons />
-        <Styled.Text>2023 Sairan Serra. All rights reserved.</Styled.Text>
+        <Styled.Text>2025 Sairan Serra. All rights reserved.</Styled.Text>
       </Styled.WrapperText>
     </Styled.ContainerMaster>
   )
